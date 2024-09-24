7ddafe0e272c366b9970b8cf723ac67e3e80f6e9
stanislavskyi <amadeusmozart1a@gmail.com>
2024-09-24 05:14:14 +0300

    Added charter screen

--- app/src/main/java/com/hfad/musicplayerapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/hfad/musicplayerapplication/presentation/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.hfad.musicplayerapplication.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hfad.musicplayerapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.menuHome -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.menuCharter -> {
+                    navController.navigate(R.id.charterFragment)
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
 }
