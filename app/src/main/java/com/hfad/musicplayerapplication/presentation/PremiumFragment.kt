edf5543e54b8be9312c1135923be2593b057c093
stanislavskyi <amadeusmozart1a@gmail.com>
2024-09-25 05:04:53 +0300

    Added account screen, premium screen

---
+++ app/src/main/java/com/hfad/musicplayerapplication/presentation/PremiumFragment.kt
@@ -0,0 +1,21 @@
+package com.hfad.musicplayerapplication.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.musicplayerapplication.R
+
+
+class PremiumFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_premium, container, false)
+    }
+
+}
